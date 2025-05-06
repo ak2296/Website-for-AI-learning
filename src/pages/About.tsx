8ac9f35f599c66a966d1b6223115aa56cf55b5e8
Elohin <mohamad.gholinia@gmail.com>
2025-05-07 00:13:11 +0200

    router installed

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <div>
+        <h1>About Our Project</h1>
+        <p>Information about the project goes here.</p>
+      </div>
+    );
+  }
+  
+  export default About;
+  
